3b425729508a9c0e7da277d354f2439e664765a5
apurvasharan17 <ranjan.apurva2017@gmail.com>
2024-09-26 23:51:10 +0530

    did some changes

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function totalQuantity(cart){
 }
 app.get('/cart/total-quantity',(req,res)=>{
   let result=totalQuantity(cart)
-  res.json({totalQuanity:result})
+  res.json(result)
 })
 function totalPrice(cart){
   let totalprice=0;
@@ -80,7 +80,7 @@ function totalPrice(cart){
 }
 app.get('/cart/total-price',(req,res)=>{
   let result=totalPrice(cart)
-  res.json({totalPrice:result})
+  res.json(result)
 })
 
 
