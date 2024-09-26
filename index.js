e5ac453fc95edf00c367809dc02a5c78bb6a9632
apurvasharan17 <ranjan.apurva2017@gmail.com>
2024-09-26 23:39:06 +0530

    cors added

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ let cart = [
   { productId: 1, name: 'Laptop', price: 50000, quantity: 1 },
   { productId: 2, name: 'Mobile', price: 20000, quantity: 2 }
 ];
-let cors = require('cors');
-
+let cors=require('cors');
 app.use(cors());
 
 function addProduct(cart,newItem){
